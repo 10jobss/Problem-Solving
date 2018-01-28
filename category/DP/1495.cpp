b2a2b06d8b8a4bbaa702779946ccd9e664d78820
Kyunghoon Jo <10jobss@gmail.com>
2018-01-28 14:43:12 +0900

    기타리스트

--- category/DP/1495.cpp
+++ category/DP/1495.cpp
@@ -1,3 +1,13 @@
+/*
+problem : https://www.acmicpc.net/problem/1495
+solution :
+	Bottom-up DP
+	cache[i][j] = j볼륨으로 i번째 곡 연주 가능 여부 (-1 : 불가능, 1 : 가능)
+	시간복잡도 O(N*M)으로 해결 가능
+	처음에 풀땐 BFS로 풀이 node는 pair<볼륨, level>
+	최악의 경우 n=100일 때 height 100의 트리가 2^100 - 1 개의 노드를 가질 수 있으므로
+	메모리는 약 (2^100 - 1)*8byte 이므로 메모리초과
+*/
 #include <cstdio>
 #include <cstring>
 #include <algorithm>

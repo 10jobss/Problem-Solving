344ffb9c9c0d599521057d8e638715461b62a924
Gyeonghun Cho <10jobss@gmail.com>
2020-05-05 18:32:45 +0900

    04/25/2020

---
+++ programmers/42885.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int anser = 0;
+    sort(people.begin(), people.end());
+    int lo = 0, hi = people.size() - 1;
+    while(lo<=hi) {
+        if(people[hi] + people[lo] <= limit)
+            lo++;
+        hi--;
+        answer++;
+    }
+    return answer;
+}
